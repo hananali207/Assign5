e8795213aadf4b1110c0279c3fe7f36dd29038d0
jasonpich1 <jason.pichette@maine.edu>
2024-03-04 13:35:22 -0500

    Finishing Touches!

--- Program5.java
+++ Program5.java
@@ -1,5 +1,6 @@
 package accidentpack;
 
+import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.Scanner;
@@ -13,23 +14,25 @@ import java.time.LocalDate;
  */
 public class Program5 {
 	
-    private static final String CSV_FILE_PATH = "/Users/hananali/eclipse-workspace/Project1DS/src/accidentpack/accidents.csv";
+    private static final String CSV_FILE_PATH = "/Users/jason/Downloads/accidents.csv";
 
     private Node root;
 
     public Program5() {
 
     }
-    Dictionary<String, Node> rootReportDict = new Hashtable<>(); 
-    
+    static Dictionary<String, Node> rootReportDict = new Hashtable<>(); 
+    static Program5 program5 = new Program5();
     
     public static void main(String[] args) throws IOException {
         Program5 program = new Program5();
         Report[] reports = Report.readReportsFromFile(CSV_FILE_PATH);
         
         System.out.println(reports.length);
-
-       
+        
+        
+		
+        //program5.printInorder(rootReportDict.get("ME"));
         	
        
         Scanner userInput = new Scanner(System.in);
@@ -38,10 +41,59 @@ public class Program5 {
         String enteredState = userInput.nextLine();
         System.out.println("Enter start date (yyyy-MM-dd):");
         String enteredStartDate = userInput.nextLine();
-     
-
-        int reportCount = program.reportsOnAndAfterDate(reports, enteredState, enteredStartDate);
-        System.out.println("Number of reports on and after " + enteredStartDate + " in state " + enteredState + ": " + reportCount);
+        ArrayList<Report>thisStatesReports = program5.stateArrayBuilder(reports, enteredState);
+        program5.populateTrees(thisStatesReports);
+        timeRecursiveFieldCount(enteredState, enteredStartDate);
+        timeRecursiveCount(enteredState, enteredStartDate);
+        timeNonRecursiveCount(reports, enteredState, enteredStartDate);
+    }
+    
+    //Times the recursive report count method and prints out results
+    protected static int timeRecursiveFieldCount(String state, String startDate){
+		long startTime = System.nanoTime();
+		int counted = program5.reportsOnAndAfterDateRecursive(rootReportDict.get(state), startDate);
+		long endTime = System.nanoTime();
+        System.out.println("Number of reports on and after " + startDate + " in state " + state + ": " + counted);
+		System.out.println((endTime - startTime)/1_000_000_000.0 + " seconds to count reports using fields.");
+		return counted;
+	}	
+    
+    //Times the recursive report (without using childrenCount fields) method count and prints out results
+    protected static int timeRecursiveCount(String state, String startDate){
+		long startTime = System.nanoTime();
+		int counted = program5.reportsOnAndAfterDateNoFields(rootReportDict.get(state), startDate);
+		long endTime = System.nanoTime();
+        System.out.println("Number of reports on and after " + startDate + " in state " + state + ": " + counted);
+		System.out.println((endTime - startTime)/1_000_000_000.0 + " seconds to count reports using no fields, recursively.");
+		return counted;
+	}	
+    
+    //Times the non-recursive report count method and prints out results
+    protected static int timeNonRecursiveCount(Report[] reports, String state, String startDate){
+		long startTime = System.nanoTime();
+		int counted = program5.reportsOnAndAfterDate(reports, state, startDate);
+		long endTime = System.nanoTime();
+        System.out.println("Number of reports on and after " + startDate + " in state " + state + ": " + counted);
+		System.out.println((endTime - startTime)/1_000_000_000.0 + " seconds to count reports using array.");
+		return counted;
+	}	
+    
+    /**
+     * Builds an ArrayList of Report objects which contains only Report objects within the given state form the entered reports
+     * 
+     * @param reports Array of all Report objects
+     * @param state State of which we want to filter reports by to receive only reports of state
+     * @return thisStateArr The ArrayList of Report objects from state
+     */
+    public ArrayList stateArrayBuilder(Report[] reports, String state) {
+    	ArrayList thisStateArr = new ArrayList<Report>();
+    	
+    	for(Report r: reports) {
+    		if(r.getState().equals(state)) {
+    			thisStateArr.add(r);
+    		}
+    	}
+    	return thisStateArr;
     }
     
 
@@ -70,19 +122,36 @@ public class Program5 {
      * @param startDate The start date to count reports from.
      * @return The number of reports on and after the given date in the subtree rooted at `root`.
      */
-    private int reportsOnAndAfterDateHelp(Node root, String startDate) {
+    private int reportsOnAndAfterDateRecursive(Node root, String startDate) {
         if (root == null)
             return 0;
 
-        //System.out.println("Checking node: " + root.report.getState() + ", " + root.report.getStartTime());
 
         if (startDate.compareTo(root.report.getStartTime()) <= 0) {
-            return root.rightChildrenCount + 1 + reportsOnAndAfterDateHelp(root.left, startDate);
+            return root.rightChildrenCount + 1 + reportsOnAndAfterDateRecursive(root.left, startDate);
         } else {
-            return reportsOnAndAfterDateHelp(root.right, startDate);
+            return reportsOnAndAfterDateRecursive(root.right, startDate);
         }
     }
+    
+    /**
+     * Counts amount of reports on or after the date entered for a BST. BST usually represents all of one state's accidents.
+     * 
+     * @param root A Node object
+     * @param startDate the date of which we want all reports on or after
+     * @return The integer count of reports on or after given date
+     */
+    private int reportsOnAndAfterDateNoFields(Node root, String startDate) {
+    	  if (root == null)
+              return 0;
 
+
+          if (startDate.compareTo(root.report.getStartTime()) <= 0) {
+              return 1 + reportsOnAndAfterDateNoFields(root.left, startDate) + reportsOnAndAfterDateNoFields(root.right, startDate);
+          } else {
+              return reportsOnAndAfterDateNoFields(root.right, startDate);
+          }
+    }
 
 
     /**
@@ -121,35 +190,28 @@ public class Program5 {
             return new Node(report);
         }
 
-        if (report.getStartTime().compareTo(root.report.getStartTime()) <= 0) {
+        if (report.getStartTime().compareTo(root.report.getStartTime()) < 0) {
+        	root.leftChildrenCount++;
+            root.left = addHelp(root.left, report);
         } else {
-            // Insert as right child only if start times are strictly equal
-            if (report.getStartTime().equals(root.report.getStartTime())) {
+            // Insert as right child if start times are strictly equal or greater than root
                 root.rightChildrenCount++;
                 root.right = addHelp(root.right, report);
-            } else {
-                root.leftChildrenCount++;
-                root.left = addHelp(root.left, report);
-            }
         }
         return root;
     }
     
+   /**
+    * Method which populates the BST for the given ArrayList, usually one states Report objects. 
+    * 
+    * @param reportArr The ArrayList<Report> to construct a BST for. 
+    */
+   public void populateTrees(ArrayList<Report> reportArr) {
+	   for(Report r : reportArr) {
+		   add(r);
+	   }
+   }
    
-
-    
-    
-   
-/*
-    public void searchForClosestAfter(LocalDate startDate, Node root) {
-    	  if (startDate.compareTo(root.report.getStartTime()) <= 0) {
-              searchForClosestAfter(startDate, root.left);
-          } else {
-              root.rightChildrenCount++;
-              root.right = addHelp(root.right, report);
-          }
-    }
-*/
     private class Node {
         Report report;
         int leftChildrenCount;
